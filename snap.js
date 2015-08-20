874cf1b060a89689fe0128b1171ebe265ecd8dae
cs <bartus.csongor@gmail.com>
2015-08-20 09:52:48 +0300

    Sense titles added

--- snap.js
+++ snap.js
@@ -1,3 +1,16 @@
+// Utils
+
+
+// Convert Title to Class
+// - What's Next -> 'whats-next'
+var convertToClassName = function(s) {
+  return s.replace(/\s+/g, '-').toLowerCase();
+}
+
+
+
+
+
 // Draw the SVG
 var snap = function(id, width, height) {
 
@@ -6,20 +19,22 @@ var snap = function(id, width, height) {
     viewBox: "0 0 " + width + " " + height
   });
 
+
+
   // Settings
   // - the number of vertical points (senses)
   var points = 8;
 
   // - the names of senses
   var senses = [
-    'position',
-    'motion',
-    'irritability',
-    'reflex',
-    'association',
-    'thought',
-    'culture',
-    'next'
+    'Position',
+    'Motion',
+    'Irritability',
+    'Reflex',
+    'Association',
+    'Thought',
+    'Culture',
+    'This metasystem transformation'
   ];
 
   // - the radius of the sense point
@@ -36,13 +51,20 @@ var snap = function(id, width, height) {
     });
   }
 
+
+
   // Draw the diagonal
   var diagonal = paper.path("M0," + height + "L" + width + ",0").attr({
     class: 'diagonal'
   });
 
-  // Draw points at the intersection of rulers with diagonal
+
+
+  // Draw sense points and circles at the intersection of rulers with diagonal
   var sensePoints = [];
+  var senseCircles = [];
+  var senseTitles = [];
+
   for (var i = 0; i < points; i++ ) {
     var intersection = Snap.path.intersection(diagonal, rulers[i]);
     var x = intersection[0].x;
@@ -54,8 +76,27 @@ var snap = function(id, width, height) {
       y -= radiusSensePoint / 2;
     }
 
+    // Class names
+    var titleConverted = convertToClassName(senses[points - i - 1]);
+    var classPoint = "sense__point sense__point--" + titleConverted;
+    var classCircle = "sense__circle sense__circle--" + titleConverted;
+    var classTitle = "sense__title sense__title--" + titleConverted;
+
+    // Draw sense points
     sensePoints[i] = paper.circle(x, y, radiusSensePoint).attr({
-      class: "sense--" + senses[points - i - 1]
+      class: classPoint
+    });
+
+    // Add text to sense points
+    senseTitles[i] = paper.text(x + (radiusSensePoint * 2), y, senses[points - i - 1]).attr({
+      class: classTitle
+    });
+
+    // Draw sense circles
+    // - radius is calculated with the Pitagoras theorem
+    var radiusCircle = Math.sqrt(Math.pow(x, 2) + Math.pow((height-y), 2));
+    senseCircles[i] = paper.circle(0, height, radiusCircle).attr({
+      class: classCircle
     });
   }
 }
