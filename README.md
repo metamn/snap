15760d421f7e5a39d76dfd61ca18e672844b7a90
cs <bartus.csongor@gmail.com>
2015-08-20 08:05:19 +0300

    Senses added

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Playing with Snap SVG
 
 http://svg.dabbles.info/
+
+* Instead of drawing with `line`, `circle` etc. better use `path`. It has richer usage later. For example paths can intersect lines don't.
--- snap.js
+++ snap.js
@@ -6,19 +6,53 @@ var snap = function(id, width, height) {
     viewBox: "0 0 " + width + " " + height
   });
 
-  // The number of vertical points
-  var points = 7;
+  // Settings
+  // - the number of vertical points (senses)
+  var points = 8;
+
+  // - the names of senses
+  var senses = [
+    'position',
+    'motion',
+    'irritability',
+    'reflex',
+    'association',
+    'thought',
+    'culture',
+    'next'
+  ];
+
+  // - the radius of the sense circle
+  var radius = 10;
+
+
 
   // Draw the vertical rulers
+  var rulers = [];
   for (var i = 1; i <= points; i++ ) {
     var y = (height / points) * i;
-    paper.line(0, y, width, y);
+    rulers[i-1] = paper.path("M0," + y + "H" + width).attr({
+      class: "ruler-" + i
+    });
   }
 
+  // Draw the diagonal
+  var diagonal = paper.path("M0," + height + "L" + width + ",0").attr({
+    class: 'diagonal'
+  });
+
+  // Draw circles at the intersection of rulers with diagonal
+  var circles = [];
+  for (var i = 0; i < points; i++ ) {
+    var intersection = Snap.path.intersection(diagonal, rulers[i]);
+    circles[i] = paper.circle(intersection[0].x, intersection[0].y, radius).attr({
+      class: "sense--" + senses[points - i - 1]
+    });
+  }
 }
 
 // Document.ready ....
 window.onload = function() {
   snap('.svg--landscape', 1366, 768);
-  snap('.svg--portrait', 768, 1024);
+  //snap('.svg--portrait', 768, 1024);
 };
